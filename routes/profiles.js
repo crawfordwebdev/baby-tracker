dd229e6ac64d79cce122e3d9a378db316982db20
Michael Crawford <crawfordwebdev@users.noreply.github.com>
2022-06-24 20:23:22 -0600

    adds profiles routes

---
+++ routes/profiles.js
@@ -0,0 +1,12 @@
+import { Router } from 'express'
+import { isLoggedIn } from '../middleware/middleware.js'
+import * as profilesCtrl from '../controllers/profiles.js'
+
+const router = Router()
+
+// GET localhost:3000/profiles/:id
+router.get('/:id', isLoggedIn, profilesCtrl.show)
+
+export {
+  router
+}
