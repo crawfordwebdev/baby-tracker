a79bc0da910af074d25b50f15148436de36be034
Michael Crawford <crawfordwebdev@users.noreply.github.com>
2022-06-27 11:13:08 -0600

    adds baby schemas

--- models/baby.js
+++ models/baby.js
@@ -2,14 +2,80 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
+const measurementSchema = new mongoose.Schema({
+  date: Date,
+  height: Number,
+  weight: Number,
+  headCircumference: Number
+}, {
+  timestamps: true
+})
+
+const miscSchema = new mongoose.Schema({
+  date: Date,
+  text: String,
+  number: Number,
+}, {
+  timestamps: true 
+})
+
+const sleepSchema = new mongoose.Schema({
+  startTime: Date,
+  endTime: Date,
+  comments: String
+}, {
+  timestamps: true 
+})
+
+const feedingSchema = new mongoose.Schema({
+  date: Date,
+  feedType: String,
+  amount: Number,
+  startTime: Date,
+  endTime: Date
+}, {
+  timestamps: true 
+})
+
+const poopSchema = new mongoose.Schema({
+  color: String,
+  consistency: String,
+  ploppable: Boolean,
+  comments: String
+}, {
+  timestamps: true 
+})
+
+const changingSchema = new mongoose.Schema({
+  date: Date,
+  diaperSize: Number,
+  diaperType: {
+    type: String,
+    enum: ["cloth", "disposable"]
+  },
+  diaperWeight: Number,
+  poop: [poopSchema], 
+  dry: Boolean,
+  wet: Boolean,
+  soiled: Boolean,
+}, {
+  timestamps: true 
+})
+
 const babySchema = new mongoose.Schema({
   caregiver: {type: Schema.Types.ObjectId, ref: "Profile"},
   name: String,
   birthday: Date,
-  sex: String
+  sex: String,
+  measurements: [measurementSchema],
+  misc: [miscSchema],
+  sleeps: [sleepSchema],
+  feedings: [feedingSchema],
+  changings: [changingSchema]
 }, {
   timestamps: true
 })
+
 
 const Baby = mongoose.model('Baby', babySchema)
 
